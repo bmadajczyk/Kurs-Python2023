2af5be0be37d791e2fe6c5ae212fb2552a40c877
Bartek <madajczykb@gmail.com>
2023-04-18 20:19:45 +0200

    Removed lines

--- 00_intro/BMI.py
+++ 00_intro/BMI.py
@@ -1,8 +1,6 @@
 w = input("Podaj wagę ->")
 h = input("Podaj wzrost ->")
-
 w = float(w.replace(",", "."))
 h = float(h.replace(",", "."))
-
 bmi = w / (h ** 2)
 print("Your BMI is:", round(bmi, 2))
